d752e4b4eda168b8bb61406fa5ad93107f17b653
John Woodell <woodie@netpress.com>
2010-03-01 21:08:53 -0800

    we need to require builder and i18n

--- lib/rails_appengine/active_support_vendored.rb
+++ lib/rails_appengine/active_support_vendored.rb
@@ -3,3 +3,5 @@ $LOAD_PATH.unshift "#{as}/builder-2.1.2"
 $LOAD_PATH.unshift "#{as}/memcache-client-1.7.4"
 $LOAD_PATH.unshift "#{as}/tzinfo-0.3.12"
 $LOAD_PATH.unshift "#{as}/i18n-0.1.3/lib"
+require 'builder'
+require 'i18n'
