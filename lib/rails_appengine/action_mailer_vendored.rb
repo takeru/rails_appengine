820101f8b83a31cdc1cff65c4e34b978b669d8d7
John Woodell <woodie@woodie-macbookpro.local>
2010-06-23 03:27:36 -0700

    work with 2.3.5 to 2.3.8

--- lib/rails_appengine/action_mailer_vendored.rb
+++ lib/rails_appengine/action_mailer_vendored.rb
@@ -1,4 +1,6 @@
-am = "#{ENV['GEM_PATH']}/gems/actionmailer-" +
-     "#{RAILS_GEM_VERSION}/lib/action_mailer/vendor"
-tmail_ver = RAILS_GEM_VERSION.eql?('2.3.5') ? 'tmail-1.2.3' : 'tmail-1.2.7'
+base = nil
+pre = File.dirname(__FILE__).split('/')[0..-4].join('/') + "/actionmailer-2.3"
+5.upto(8) { |v| base = "#{pre}.#{v}" if File.exist?("#{pre}.#{v}") }
+am = "#{base}/lib/action_mailer/vendor"
+tmail_ver = base[-5..-1].eql?('2.3.5') ? 'tmail-1.2.3' : 'tmail-1.2.7'
 $LOAD_PATH.unshift "#{am}/#{tmail_ver}"
--- lib/rails_appengine/active_support_vendored.rb
+++ lib/rails_appengine/active_support_vendored.rb
@@ -1,6 +1,8 @@
-as = "#{ENV['GEM_PATH']}/gems/activesupport-" +
-     "#{RAILS_GEM_VERSION}/lib/active_support/vendor"
-i18n_ver = RAILS_GEM_VERSION.eql?('2.3.5') ? 'i18n-0.1.3/lib' : 'i18n-0.3.7'
+base = nil
+pre = File.dirname(__FILE__).split('/')[0..-4].join('/') + "/activesupport-2.3"
+5.upto(8) { |v| base = "#{pre}.#{v}" if File.exist?("#{pre}.#{v}") }
+as = "#{base}/lib/active_support/vendor"
+i18n_ver = base[-5..-1].eql?('2.3.5') ? 'i18n-0.1.3/lib' : 'i18n-0.3.7'
 $LOAD_PATH.unshift "#{as}/builder-2.1.2"
 $LOAD_PATH.unshift "#{as}/memcache-client-1.7.4"
 $LOAD_PATH.unshift "#{as}/tzinfo-0.3.12"
