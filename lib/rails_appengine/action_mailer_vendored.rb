724425208968ec615a03fed91203475b0156f8fb
John Woodell <woodie@woodie-macbookpro.local>
2010-09-05 03:58:23 -0700

    support Rails 2.3.9

--- lib/rails_appengine/action_mailer_vendored.rb
+++ lib/rails_appengine/action_mailer_vendored.rb
@@ -1,6 +1,6 @@
 base = nil
 pre = File.dirname(__FILE__).split('/')[0..-4].join('/') + "/actionmailer-2.3"
-5.upto(8) { |v| base = "#{pre}.#{v}" if File.exist?("#{pre}.#{v}") }
+5.upto(9) { |v| base = "#{pre}.#{v}" if File.exist?("#{pre}.#{v}") }
 am = "#{base}/lib/action_mailer/vendor"
 tmail_ver = base[-5..-1].eql?('2.3.5') ? 'tmail-1.2.3' : 'tmail-1.2.7'
 $LOAD_PATH.unshift "#{am}/#{tmail_ver}"
--- lib/rails_appengine/active_support_vendored.rb
+++ lib/rails_appengine/active_support_vendored.rb
@@ -1,11 +1,12 @@
 base = nil
 pre = File.dirname(__FILE__).split('/')[0..-4].join('/') + "/activesupport-2.3"
-5.upto(8) { |v| base = "#{pre}.#{v}" if File.exist?("#{pre}.#{v}") }
+5.upto(9) { |v| base = "#{pre}.#{v}" if File.exist?("#{pre}.#{v}") }
 as = "#{base}/lib/active_support/vendor"
-i18n_ver = base[-5..-1].eql?('2.3.5') ? 'i18n-0.1.3/lib' : 'i18n-0.3.7'
+iv = {'2.3.5' => '0.1.3/lib', '2.3.8' => '0.3.7', '2.3.9' => '0.4.1'}
+i18n_ver = iv[base[-5..-1]]
 $LOAD_PATH.unshift "#{as}/builder-2.1.2"
 $LOAD_PATH.unshift "#{as}/memcache-client-1.7.4"
 $LOAD_PATH.unshift "#{as}/tzinfo-0.3.12"
-$LOAD_PATH.unshift "#{as}/#{i18n_ver}"
+$LOAD_PATH.unshift "#{as}/i18n-#{i18n_ver}"
 require 'builder'
 require 'i18n'
